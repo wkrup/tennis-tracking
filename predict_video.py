d6467f5339d4a43df37acb3fd0b80ba43e5c03be
wkrup <tpfpsgoyo@gmail.com>
2024-02-21 14:49:25 +0900

    change_v3

--- predict_video.py
+++ predict_video.py
@@ -14,6 +14,7 @@ import time
 from sktime.datatypes._panel._convert import from_2d_array_to_nested
 from court_detector import CourtDetector
 from Models.tracknet import trackNet
+from Models.TrackNet3 import TrackNet
 from TrackPlayers.trackplayers import *
 from utils import get_video_properties, get_dtype
 from detection import *
@@ -36,7 +37,7 @@ minimap = args.minimap
 bounce = args.bounce
 
 n_classes = 256
-save_weights_path = 'WeightsTracknet/model.1'
+save_weights_path = 'WeightsTracknet/model.3'
 yolo_classes = 'Yolov3/yolov3.txt'
 yolo_weights = 'Yolov3/yolov3.weights'
 yolo_config = 'Yolov3/yolov3.cfg'
@@ -67,10 +68,10 @@ width, height = 640, 360
 img, img1, img2 = None, None, None
 
 # load TrackNet model
-modelFN = trackNet
-m = modelFN(n_classes, input_height=height, input_width=width)
-m.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-m.load_weights(save_weights_path)
+# modelFN = TrackNet
+# m = modelFN(n_classes, input_height=height, input_width=width)
+# m.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+# m.load_weights(save_weights_path)
 
 # In order to draw the trajectory of tennis, we need to save the coordinate of previous 7 frames
 q = queue.deque()
@@ -131,98 +132,167 @@ player1_boxes = detection_model.player_1_boxes
 player2_boxes = detection_model.player_2_boxes
 
 video = cv2.VideoCapture(input_video_path)
-frame_i = 0
+output_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+output_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-last = time.time() # start counting 
-# while (True):
-for img in frames:
-    print('Tracking the ball: {}'.format(round( (currentFrame / total) * 100, 2)))
-    frame_i += 1
+#start from first frame
+currentFrame = 0
 
-    # detect the ball
-    # img is the frame that TrackNet will predict the position
-    # since we need to change the size and type of img, copy it to output_img
+#width and height in TrackNet
+width , height = 640, 360
+img, img1, img2 = None, None, None
+
+#load TrackNet model
+modelFN = TrackNet
+m = modelFN( n_classes , input_height=height, input_width=width   )
+m.compile(loss='categorical_crossentropy', optimizer= 'adadelta' , metrics=['accuracy'])
+m.load_weights(  save_weights_path  )
+
+# In order to draw the trajectory of tennis, we need to save the coordinate of preious 7 frames 
+q = queue.deque()
+for i in range(0,8):
+	q.appendleft(None)
+
+#save prediction images as vidoe
+#Tutorial: https://stackoverflow.com/questions/33631489/error-during-saving-a-video-using-python-and-opencv
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_video = cv2.VideoWriter(output_video_path,fourcc, fps, (output_width,output_height))
+
+
+#both first and second frames cant be predict, so we directly write the frames to output video
+#capture frame-by-frame
+video.set(1,currentFrame); 
+ret, img1 = video.read()
+#write image to video
+output_video.write(img1)
+currentFrame +=1
+#resize it 
+img1 = cv2.resize(img1, ( width , height ))
+#input must be float type
+img1 = img1.astype(np.float32)
+
+#capture frame-by-frame
+video.set(1,currentFrame);
+ret, img = video.read()
+#write image to video
+output_video.write(img)
+currentFrame +=1
+#resize it 
+img = cv2.resize(img, ( width , height ))
+#input must be float type
+img = img.astype(np.float32)
+
+coords.append(None)
+coords.append(None)
+last = time.time()
+t.append(time.time()-last)
+t.append(time.time()-last)
+
+last = time.time()
+while(True):
+
+    img2 = img1
+    img1 = img
+
+    #capture frame-by-frame
+    video.set(1,currentFrame); 
+    ret, img = video.read()
+    img = frames[currentFrame-1]
+
+    #if there dont have any frame in video, break
+    if not ret: 
+        break
+
+    #img is the frame that TrackNet will predict the position
+    #since we need to change the size and type of img, copy it to output_img
     output_img = img
 
-    # resize it
-    img = cv2.resize(img, (width, height))
-    # input must be float type
+    #resize it 
+    img = cv2.resize(img, ( width , height ))
+    #input must be float type
     img = img.astype(np.float32)
 
-    # since the odering of TrackNet  is 'channels_first', so we need to change the axis
-    X = np.rollaxis(img, 2, 0)
-    # prdict heatmap
-    pr = m.predict(np.array([X]))[0]
 
-    # since TrackNet output is ( net_output_height*model_output_width , n_classes )
-    # so we need to reshape image as ( net_output_height, model_output_width , n_classes(depth) )
-    pr = pr.reshape((height, width, n_classes)).argmax(axis=2)
+    #combine three imgs to  (width , height, rgb*3)
+    X =  np.concatenate((img, img1, img2),axis=2)
 
-    # cv2 image must be numpy.uint8, convert numpy.int64 to numpy.uint8
-    pr = pr.astype(np.uint8)
+    #since the odering of TrackNet  is 'channels_first', so we need to change the axis
+    X = np.rollaxis(X, 2, 0)
+    #prdict heatmap
+    pr = m.predict( np.array([X]) )[0]
 
-    # reshape the image size as original input image
-    heatmap = cv2.resize(pr, (output_width, output_height))
+    #since TrackNet output is ( net_output_height*model_output_width , n_classes )
+    #so we need to reshape image as ( net_output_height, model_output_width , n_classes(depth) )
+    #.argmax( axis=2 ) => select the largest probability as class
 
-    # heatmap is converted into a binary image by threshold method.
-    ret, heatmap = cv2.threshold(heatmap, 127, 255, cv2.THRESH_BINARY)
 
-    # find the circle in image with 2<=radius<=7
-    circles = cv2.HoughCircles(heatmap, cv2.HOUGH_GRADIENT, dp=1, minDist=1, param1=50, param2=2, minRadius=2,
-                              maxRadius=7)
+    pr = pr.reshape(( height ,  width , n_classes ) ).argmax( axis=2 )
+    
 
+    #cv2 image must be numpy.uint8, convert numpy.int64 to numpy.uint8
+    pr = pr.astype(np.uint8) 
+    
+
+    #reshape the image size as original input image
+    heatmap = cv2.resize(pr  , (output_width, output_height ))
+
+    #heatmap is converted into a binary image by threshold method.
+    ret,heatmap = cv2.threshold(heatmap,127,255,cv2.THRESH_BINARY)
+
+    #find the circle in image with 2<=radius<=7
+    circles = cv2.HoughCircles(heatmap, cv2.HOUGH_GRADIENT,dp=1,minDist=1,param1=50,param2=2,minRadius=2,maxRadius=7)
 
     output_img = mark_player_box(output_img, player1_boxes, currentFrame-1)
     output_img = mark_player_box(output_img, player2_boxes, currentFrame-1)
-    
-    PIL_image = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
-    PIL_image = Image.fromarray(PIL_image)
 
+    #In order to draw the circle in output_img, we need to used PIL library
+    #Convert opencv image format to PIL image format
+    PIL_image = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)   
+    PIL_image = Image.fromarray(PIL_image)
     # check if there have any tennis be detected
+
     if circles is not None:
-        # if only one tennis be detected
+        #if only one tennis be detected
         if len(circles) == 1:
 
             x = int(circles[0][0][0])
             y = int(circles[0][0][1])
-
-            coords.append([x,y])
+            print(currentFrame, x,y)
+            coords.append([x, y])
             t.append(time.time()-last)
 
-            # push x,y to queue
-            q.appendleft([x, y])
-            # pop x,y from queue
-            q.pop()
-
+            #push x,y to queue
+            q.appendleft([x,y])   
+            #pop x,y from queue
+            q.pop()    
         else:
+            #push None to queue
+            q.appendleft(None)
+            #pop x,y from queue
+            q.pop()
             coords.append(None)
             t.append(time.time()-last)
-            # push None to queue
-            q.appendleft(None)
-            # pop x,y from queue
-            q.pop()
-
     else:
+        #push None to queue
+        q.appendleft(None)
+        #pop x,y from queue
+        q.pop()
         coords.append(None)
         t.append(time.time()-last)
-        # push None to queue
-        q.appendleft(None)
-        # pop x,y from queue
-        q.pop()
 
-    # draw current frame prediction and previous 7 frames as yellow circle, total: 8 frames
-    for i in range(0, 8):
+    #draw current frame prediction and previous 7 frames as yellow circle, total: 8 frames
+    for i in range(0,8):
         if q[i] is not None:
             draw_x = q[i][0]
             draw_y = q[i][1]
-            bbox = (draw_x - 2, draw_y - 2, draw_x + 2, draw_y + 2)
+
+            bbox =  (draw_x - 2, draw_y - 2, draw_x + 2, draw_y + 2)
             draw = ImageDraw.Draw(PIL_image)
-            draw.ellipse(bbox, outline='yellow')
+            draw.ellipse(bbox, outline ='yellow')
             del draw
 
-    # Convert PIL image format back to opencv image format
+  # Convert PIL image format back to opencv image format
     opencvImage = cv2.cvtColor(np.array(PIL_image), cv2.COLOR_RGB2BGR)
-
     output_video.write(opencvImage)
 
     # next frame
@@ -249,6 +319,8 @@ if minimap == 1:
   remove_outliers(x, y, coords)
   # Interpolation
   coords = interpolation(coords)
+  print("coords: ", len(coords))
+  print("frames: ", len(frames))
   create_top_view(court_detector, detection_model, coords, fps)
   minimap_video = cv2.VideoCapture('VideoOutput/minimap.mp4')
   fps2 = int(minimap_video.get(cv2.CAP_PROP_FPS))
@@ -330,6 +402,13 @@ if bounce == 1:
   Vs = from_2d_array_to_nested(Vs.to_numpy())
 
   X = pd.concat([Xs, Ys, Vs], 1)
+  # print("Xs: ", Xs)
+  # print("Ys: ", Ys)
+  # print("Vs: ", Vs)
+  # print("X: ", X)
+
+  X.to_csv("X_save.csv", save_index=False)
+  X = X.fillna(value=np.nan)
 
   # load the pre-trained classifier  
   clf = load(open('clf.pkl', 'rb'))
